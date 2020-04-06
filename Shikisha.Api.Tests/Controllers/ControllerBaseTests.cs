8971760271b69c7b0136a820969a3b31c227acd0
Rogue Elephant <jup.trandell@gmail.com>
2020-04-06 07:59:48 +0100

    Added controlle rfailure tests

--- Shikisha.Api.Tests/Controllers/ControllerBaseTests.cs
+++ Shikisha.Api.Tests/Controllers/ControllerBaseTests.cs
@@ -33,23 +33,29 @@ namespace Shikisha.Tests.Controllers
             _mockObjectList = mockObjectList ?? Enumerable.Range(0, 5).Select(x => generateMockObject()).ToList();
         }
 
-        protected async Task<ActionResult<ServiceResponse<T>>> SuccessTest<T>(T mockResult,
+        protected async Task<ActionResult<ServiceResponse<T>>> BaseTest<T, TObjectResult>(ServiceResponse<T> mockResponse,
+        int expectedStatusCode,
         Expression<Func<IService<TEntity>, Task<ServiceResponse<T>>>> methodToCall,
         Func<ApiControllerBase<TEntity>, Task<ActionResult<ServiceResponse<T>>>> controllerAction)
+        where TObjectResult : ObjectResult
         {
-            var mockResponse = new ServiceResponse<T>(mockResult);
-            
             _mockService.Setup(methodToCall).ReturnsAsync(mockResponse);
 
             var result = await controllerAction(_controller);
 
             var actionResult = Assert.IsType<ActionResult<ServiceResponse<T>>>(result);
-            var okResult = Assert.IsType<OkObjectResult>(result.Result);
-            Assert.Equal(200, okResult.StatusCode);
-            Assert.Equal(mockResponse, okResult.Value);
             _mockService.Verify(methodToCall, Times.Once);
+            var objectResult = Assert.IsType<TObjectResult>(result.Result);
+            Assert.Equal(expectedStatusCode, objectResult.StatusCode);
+            Assert.Equal(mockResponse, objectResult.Value);
             return result;
         }
+
+        #region Success Tests
+        protected async Task<ActionResult<ServiceResponse<T>>> SuccessTest<T>(T mockResult,
+        Expression<Func<IService<TEntity>, Task<ServiceResponse<T>>>> methodToCall,
+        Func<ApiControllerBase<TEntity>, Task<ActionResult<ServiceResponse<T>>>> controllerAction)
+            => await BaseTest<T, OkObjectResult>(new ServiceResponse<T>(mockResult), 200, methodToCall, controllerAction);
 
         [Fact]
         public async Task<ActionResult<ServiceResponse<List<TEntity>>>> Fact_BaseGetAll_Success()
@@ -66,5 +72,31 @@ namespace Shikisha.Tests.Controllers
         [Fact]
         public async Task<ActionResult<ServiceResponse<TEntity>>> Fact_BaseUpdate_Success()
             => await SuccessTest(_mockObject, service => service.Update(_mockId, _mockObject), controller => controller.Update(_mockId, _mockObject));
+
+        #endregion Success Tests
+
+        #region Failure Tests
+        protected async Task<ActionResult<ServiceResponse<T>>> FailureTest<T>(T mockResult,
+        Expression<Func<IService<TEntity>, Task<ServiceResponse<T>>>> methodToCall,
+        Func<ApiControllerBase<TEntity>, Task<ActionResult<ServiceResponse<T>>>> controllerAction)
+            => await BaseTest<T, BadRequestObjectResult>(new ServiceResponse<T>(new List<ServiceError>(){new ServiceError("Test Error","Test Error")}),
+            400, methodToCall, controllerAction);
+
+        [Fact]
+        public async Task<ActionResult<ServiceResponse<List<TEntity>>>> Fact_BaseGetAll_Failure()
+            => await FailureTest(_mockObjectList, service => service.GetAll(), controller => controller.Get());
+
+        [Fact]
+        public async Task<ActionResult<ServiceResponse<TEntity>>> Fact_BaseGetById_Failure()
+            => await FailureTest(_mockObject, service => service.GetById(_mockId, false), controller => controller.GetById(_mockId, false));
+
+        [Fact]
+        public async Task<ActionResult<ServiceResponse<TEntity>>> Fact_BaseAdd_Failure()
+            => await FailureTest(_mockObject, service => service.Add(_mockObject), controller => controller.Add(_mockObject));
+
+        [Fact]
+        public async Task<ActionResult<ServiceResponse<TEntity>>> Fact_BaseUpdate_Failure()
+            => await FailureTest(_mockObject, service => service.Update(_mockId, _mockObject), controller => controller.Update(_mockId, _mockObject));
+        #endregion Failure Tests
     }
 }
